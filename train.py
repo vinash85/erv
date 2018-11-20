28d4b9940652edd953ea3e0bcc4d4ded637e5d77
vinash85 <avinash.sahu@gmail.com>
2018-11-19 23:32:08 -0500

    initial setup

--- train.py
+++ train.py
@@ -28,38 +28,6 @@ train_batch_size = 256
 
 # Read feature files
 
-# data_dir = "/home/as892/project/icb/data/"
-data_dir = "../../data/ssgsea/"
-# data_dir = "../../results/simulation/"
-job_dir_prefix = "../../results/model/"
-
-
-job_dir = job_dir_prefix + "pytorch_ssgsea_" + timestr
-
-
-def add2stringlist(prefix, List):
-    return [prefix + elem for elem in List]
-
-
-# train_files = add2stringlist(data_dir, ["survival.data.txt", "survival.train.txt"])
-# test_files = add2stringlist(data_dir, ["survival.data.txt", "survival.train.txt"])
-train_files = add2stringlist(data_dir, ["tcga_ssgsea_train.txt", "tcga_survival_train.txt"])
-test_files = add2stringlist(data_dir, ["tcga_ssgsea_test.txt", "tcga_survival_test.txt"])
-
-
-train_features = gn.readFile(train_files[0])
-test_features = gn.readFile(test_files[0])
-
-# Read labels
-train_labels = gn.readFile(train_files[1])
-test_labels = gn.readFile(test_files[1])
-
-train_steps_gen, train_input_size, train_generator = gn.generator_survival(
-    train_features, train_labels, shuffle=True, batch_size=train_batch_size)
-test_steps_gen, test_input_size, val_generator = gn.generator_survival(
-    test_features, test_labels, shuffle=True, batch_size=train_batch_size)
-
-input_size = train_input_size
 
 # define concordance index
 
@@ -210,13 +178,142 @@ def train(model, optimizer, loss_fn, dataloader, metrics, params):
     logging.info("- Train metrics: " + metrics_string)
 
 
-model = NeuralNet(input_size, hidden_size, num_classes).double().to(device)
+def train_and_evaluate(model, train_dataloader, val_dataloader, optimizer, loss_fn, metrics, params, model_dir,
+                       restore_file=None):
+    """Train the model and evaluate every epoch.
+    Args:
+        model: (torch.nn.Module) the neural network
+        train_dataloader: (DataLoader) a torch.utils.data.DataLoader object that fetches training data
+        val_dataloader: (DataLoader) a torch.utils.data.DataLoader object that fetches validation data
+        optimizer: (torch.optim) optimizer for parameters of model
+        loss_fn: a function that takes batch_output and batch_labels and computes the loss for the batch
+        metrics: (dict) a dictionary of functions that compute a metric using the output and labels of each batch
+        params: (Params) hyperparameters
+        model_dir: (string) directory containing config, weights and log
+        restore_file: (string) optional- name of file to restore from (without its extension .pth.tar)
+    """
+    # reload weights from restore_file if specified
+    if restore_file is not None:
+        restore_path = os.path.join(args.model_dir, args.restore_file + '.pth.tar')
+        logging.info("Restoring parameters from {}".format(restore_path))
+        utils.load_checkpoint(restore_path, model, optimizer)
+
+    best_val_acc = 0.5  # for confidence
+
+    for epoch in range(params.num_epochs):
+        # Run one epoch
+        logging.info("Epoch {}/{}".format(epoch + 1, params.num_epochs))
+
+        # compute number of batches in one epoch (one full pass over the training set)
+        train(model, optimizer, loss_fn, train_dataloader, metrics, params)
+
+        # Evaluate for one epoch on validation set
+        val_metrics = evaluate(model, loss_fn, val_dataloader, metrics, params)
+
+        val_acc = val_metrics['c_index']
+        is_best = val_acc >= best_val_acc
+
+        # Save weights
+        utils.save_checkpoint({'epoch': epoch + 1,
+                               'state_dict': model.state_dict(),
+                               'optim_dict': optimizer.state_dict()},
+                              is_best=is_best,
+                              checkpoint=model_dir)
+
+        # If best_eval, best_save_path
+        if is_best:
+            logging.info("- Found new best cindex")
+            best_val_acc = val_acc
+
+            # Save best val metrics in a json file in the model directory
+            best_json_path = os.path.join(model_dir, "metrics_val_best_weights.json")
+            utils.save_dict_to_json(val_metrics, best_json_path)
+
+        # Save latest val metrics in a json file in the model directory
+        last_json_path = os.path.join(model_dir, "metrics_val_last_weights.json")
+        utils.save_dict_to_json(val_metrics, last_json_path)
+        # return the trained model
+
+
+if __name__ == '__main__':
+
+    # Load the parameters from json file
+    args = parser.parse_args()
+    json_path = os.path.join(args.model_dir, 'params.json')
+    assert os.path.isfile(json_path), "No json configuration file found at {}".format(json_path)
+    params = utils.Params(json_path)
+
+    # use GPU if available
+    params.cuda = torch.cuda.is_available()
+
+    # Set the random seed for reproducible experiments
+    torch.manual_seed(230)
+    if params.cuda:
+        torch.cuda.manual_seed(230)
+
+    # Set the logger
+    utils.set_logger(os.path.join(args.model_dir, 'train.log'))
+
+    # Create the input data pipeline
+    logging.info("Loading the datasets...")
+
+    # # data_dir = "/home/as892/project/icb/data/"
+    # data_dir = "../../data/ssgsea/"
+    # # data_dir = "../../results/simulation/"
+    # job_dir_prefix = "../../results/model/"
+
+    # job_dir = job_dir_prefix + "pytorch_ssgsea_" + timestr
+
+    def add2stringlist(prefix, List):
+        return [prefix + elem for elem in List]
+
+    # train_files = add2stringlist(data_dir, ["survival.data.txt", "survival.train.txt"])
+    # test_files = add2stringlist(data_dir, ["survival.data.txt", "survival.train.txt"])
+    train_files = add2stringlist(data_dir, ["tcga_ssgsea_train.txt", "tcga_survival_train.txt"])
+    test_files = add2stringlist(data_dir, ["tcga_ssgsea_test.txt", "tcga_survival_test.txt"])
+
+    train_features = gn.readFile(train_files[0])
+    test_features = gn.readFile(test_files[0])
+
+    # Read labels
+    train_labels = gn.readFile(train_files[1])
+    test_labels = gn.readFile(test_files[1])
+
+    train_steps_gen, train_input_size, train_generator = gn.generator_survival(
+        train_features, train_labels, shuffle=True, batch_size=train_batch_size)
+    test_steps_gen, test_input_size, val_generator = gn.generator_survival(
+        test_features, test_labels, shuffle=True, batch_size=train_batch_size)
+
+    input_size = train_input_size
+
+    # fetch dataloaders
+    # dataloaders = data_loader.fetch_dataloader(['train', 'val'], args.data_dir, params)
+    train_dl = train_generator
+    val_dl = val_generator
+
+    logging.info("- done.")
+
+    # Define the model and optimizer
+    model = net.FCN(params).cuda() if params.cuda else net.FCN(params)
+    optimizer = optim.Adam(model.parameters(), lr=params.learning_rate)
+
+    # fetch loss function and metrics
+    loss_fn = net.negative_log_partial_likelihood
+    metrics = net.metrics
+
+    # Train the model
+    logging.info("Starting training for {} epoch(s)".format(params.num_epochs))
+    train_and_evaluate(model, train_dl, val_dl, optimizer, loss_fn, metrics, params, args.model_dir,
+                       args.restore_file)
+
+
+# model = NeuralNet(input_size, hidden_size, num_classes).double().to(device)
 
 # model = ConvNet(num_classes).to(device)
 
 # Loss and optimizer
 # criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+# optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 # Train the model
 # total_step = len(train_loader)

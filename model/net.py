4ca0e451cfc2bee2a5f8145819a4d316dcaf0c95
vinash85 <avinash.sahu@gmail.com>
2018-11-21 11:00:07 -0500

    buggy commit

--- model/net.py
+++ model/net.py
@@ -4,6 +4,47 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from lifelines.utils import concordance_index
+
+# Fully connected neural network with one hidden layer
+
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNet, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
+
+
+# Convolutional neural network (two convolutional layers)
+class ConvNet(nn.Module):
+    def __init__(self, num_classes=10):
+        super(ConvNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc = nn.Linear(7 * 7 * 32, num_classes)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        return out
 
 
 class Net(nn.Module):
@@ -33,21 +74,21 @@ class Net(nn.Module):
         """
         super(Net, self).__init__()
         self.num_channels = params.num_channels
-        
+
         # each of the convolution layers below have the arguments (input_channels, output_channels, filter_size,
         # stride, padding). We also include batch normalisation layers that help stabilise training.
         # For more details on how to use these layers, check out the documentation.
         self.conv1 = nn.Conv2d(3, self.num_channels, 3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(self.num_channels)
-        self.conv2 = nn.Conv2d(self.num_channels, self.num_channels*2, 3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(self.num_channels*2)
-        self.conv3 = nn.Conv2d(self.num_channels*2, self.num_channels*4, 3, stride=1, padding=1)
-        self.bn3 = nn.BatchNorm2d(self.num_channels*4)
+        self.conv2 = nn.Conv2d(self.num_channels, self.num_channels * 2, 3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(self.num_channels * 2)
+        self.conv3 = nn.Conv2d(self.num_channels * 2, self.num_channels * 4, 3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(self.num_channels * 4)
 
         # 2 fully connected layers to transform the output of the convolution layers to the final output
-        self.fc1 = nn.Linear(8*8*self.num_channels*4, self.num_channels*4)
-        self.fcbn1 = nn.BatchNorm1d(self.num_channels*4)
-        self.fc2 = nn.Linear(self.num_channels*4, 6)       
+        self.fc1 = nn.Linear(8 * 8 * self.num_channels * 4, self.num_channels * 4)
+        self.fcbn1 = nn.BatchNorm1d(self.num_channels * 4)
+        self.fc2 = nn.Linear(self.num_channels * 4, 6)
         self.dropout_rate = params.dropout_rate
 
     def forward(self, s):
@@ -72,11 +113,11 @@ class Net(nn.Module):
         s = F.relu(F.max_pool2d(s, 2))                      # batch_size x num_channels*4 x 8 x 8
 
         # flatten the output for each image
-        s = s.view(-1, 8*8*self.num_channels*4)             # batch_size x 8*8*num_channels*4
+        s = s.view(-1, 8 * 8 * self.num_channels * 4)             # batch_size x 8*8*num_channels*4
 
         # apply 2 fully connected layers with dropout
-        s = F.dropout(F.relu(self.fcbn1(self.fc1(s))), 
-            p=self.dropout_rate, training=self.training)    # batch_size x self.num_channels*4
+        s = F.dropout(F.relu(self.fcbn1(self.fc1(s))),
+                      p=self.dropout_rate, training=self.training)    # batch_size x self.num_channels*4
         s = self.fc2(s)                                     # batch_size x 6
 
         # apply log softmax on each image's output (this is recommended over applying softmax
@@ -99,7 +140,45 @@ def loss_fn(outputs, labels):
           demonstrates how you can easily define a custom loss function.
     """
     num_examples = outputs.size()[0]
-    return -torch.sum(outputs[range(num_examples), labels])/num_examples
+    return -torch.sum(outputs[range(num_examples), labels]) / num_examples
+
+
+def negative_log_partial_likelihood(censor, risk):
+    """Return the negative log-partial likelihood of the prediction
+    y_true contains the survival time
+    risk is the risk output from the neural network
+    censor is the vector of inputs that are censored
+    regularization is the regularization constant (not used currently in model)
+
+    Uses the torch backend to perform calculations
+
+    Sorts the surv_time by sorted reverse time
+    """
+
+    # calculate negative log likelihood from estimated risk
+    epsilon = 0.00001
+    risk = torch.reshape(risk, [-1])  # flatten
+    hazard_ratio = torch.exp(risk)
+
+    # cumsum on sorted surv time accounts for concordance
+    log_risk = torch.log(torch.cumsum(hazard_ratio, dim=0) + epsilon)
+    log_risk = torch.reshape(log_risk, [-1])
+    uncensored_likelihood = risk - log_risk
+
+    # apply censor mask: 1 - dead, 0 - censor
+    censored_likelihood = uncensored_likelihood * censor
+    num_observed_events = torch.sum(censor)
+    neg_likelihood = - torch.sum(censored_likelihood) / \
+        num_observed_events
+    # print(type(neg_likelihood))
+
+    if (neg_likelihood != neg_likelihood):
+        print(neg_likelihood)
+        print(censor[np.isnan(censor)])
+        print(risk[np.isnan(risk)])
+        raise ValueError("nan found")
+
+    return neg_likelihood
 
 
 def accuracy(outputs, labels):
@@ -113,11 +192,21 @@ def accuracy(outputs, labels):
     Returns: (float) accuracy in [0,1]
     """
     outputs = np.argmax(outputs, axis=1)
-    return np.sum(outputs==labels)/float(labels.size)
+    return np.sum(outputs == labels) / float(labels.size)
+
+
+def c_index(predicted_risk, survival):
+    # calculate the concordance index
+    survival_time, censor = survival[:, 0], survival[:, 1]
+    epsilon = 0.001
+    partial_hazard = np.exp(-(predicted_risk + epsilon))
+    censor = censor.astype(int)
+    ci = concordance_index(survival_time, partial_hazard, censor)
+    return ci
 
 
 # maintain all metrics required in this dictionary- these are used in the training and evaluation loops
 metrics = {
-    'accuracy': accuracy,
+    'c_index': c_index,
     # could add more metrics such as accuracy for each token type
 }

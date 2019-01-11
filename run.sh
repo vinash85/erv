c74403d6f124a04bacdb16c2f23749bcff52ef23
vinash85 <avinash.sahu@gmail.com>
2019-01-11 16:06:33 +0530

    -m

--- run.sh
+++ run.sh
@@ -4,3 +4,6 @@ model_dir="/homes6/asahu/project/deeplearning/icb/data/ssgsea/models/"
 # python train.py --data_dir ~/Dropbox/project/code/deeplearning/icb/results/simulation/datasets
 python train.py --data_dir ~/Dropbox/project/code/deeplearning/icb/results/simulation5jan/datasets
 python train.py --data_dir $data_dir --model_dir $model_dir
+
+
+python train.py --data_dir ~/local_data/processed/datasets/ --model_dir /homes6/asahu/project/deeplearning/icb/deepImmune/experiments/tcga_43

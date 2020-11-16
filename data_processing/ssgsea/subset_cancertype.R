8f6b35c40b916e726cd8d89a856dae8cea2b7b5b
Avinash <asahu@kraken1.cm.cluster>
2020-11-15 22:04:01 -0500

    temp

--- data_processing/ssgsea/subset_cancertype.R
+++
@@ -1,9 +0,0 @@
-setwd("/Users/avi/projects/deepImmune/data/tcga/neoantigen.v2/attention/tcga.imputed/brca")
-aa = fread("../dataset_train.txt")
-bb = aa[cancertype=="BRCA"]
-write.table(file="dataset_train.txt", x = bb,
-		row.names = F, col.names =T,  sep="\t", quote=F )
-aa = fread("../dataset_val.txt")
-bb = aa[cancertype=="BRCA"]
-write.table(file="dataset_val.txt", x = bb,
-		row.names = F, col.names =T,  sep="\t", quote=F )
--- embedding_notes.md
+++
@@ -1,5 +0,0 @@
-### Notes of embedding structure
-
-* layers = number of layers per blocks -- should be a list
-* blocks = number of blocks 
-* kernel_size
